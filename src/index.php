ac2944bfeaf93a8f54e8a003f4174386cbf40b69
Dewei Liu <deweiliu@hotmail.com>
2020-03-07 23:06:07 +0000

    improve layout

--- src/index.php
+++ src/index.php
@@ -15,9 +15,7 @@
 
         <h1><a href="/">Dewei Liu</a></h1>
         <p>Hi, welcome to my personal website.</p>
-        <br>
         <p>Please view my <a href="resume/Dewei Liu CV.pdf">resume</a> and <a href="resume/Dewei Liu transcript.pdf">academic transcript</a>.</p>
-        <br>
         <p>Below are my on-going projects</p>
         <p><a href="http://repeater.deweiliu.com/">Webpage Repeater</a> - A service that helps you browse the Internet
             without revealing your IP address</p>
@@ -28,17 +26,17 @@
         <p>You can also follow me via the following sites.</p>
         <p><a href="https://www.linkedin.com/in/deweiliu/">LinkedIn</a></p>
         <p><a href="https://github.com/deweiliu">GitHub</a></p>
+        <br>
+        <br>
 
-        <div style="bottom: 1%;left: 1%;position: absolute;">
-            <p>Updated on
-                <?php
-                $time = getenv('UPDATE_TIME');
-                echo date("l d F Y", $time);
-                ?>
-            </p>
-            <p>Thank you :)</p>
-            <img src="/images/favicon.png" width="10%" height="10%">
-        </div>
+        <p>Updated on
+            <?php
+            $time = getenv('UPDATE_TIME');
+            echo date("l d F Y", $time);
+            ?>
+        </p>
+        <p>Thank you :)</p>
+        <img src="/images/favicon.png" width="10%" height="10%">
     </div>
 </body>
 
