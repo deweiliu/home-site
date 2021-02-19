c89e74e74d34aabe6c2b663ebe2b989be74d9b99
Dewei Liu <deweiliu@hotmail.com>
2021-02-19 23:24:35 +0000

    Add AWS badge

--- src/index.php
+++ src/index.php
@@ -13,6 +13,11 @@
 <body>
     <div class="w3-container">
 
+        <div style="float: right;">
+            <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="af9d08bc-3db5-4d8d-abe5-fbfda1b28b2f" data-share-badge-host="https://www.youracclaim.com"></div>
+            <script type="text/javascript" async src="//cdn.youracclaim.com/assets/utilities/embed.js"></script>
+        </div>
+
         <h1><a href="/">Dewei Liu</a></h1>
 
         <p>Hi, welcome to my personal website.</p>
@@ -37,6 +42,7 @@
         <p><a href="https://github.com/deweiliu">GitHub</a></p>
         <br>
         <br>
+
 
         <p>Updated on
             <?php
