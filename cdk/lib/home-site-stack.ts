c53c6e98d54a5337a5fbc5a1f4cf100714d8f767
Dewei Liu <42348814+deweiliu@users.noreply.github.com>
2021-10-26 23:35:13 +0100

    Add cdk resources

--- cdk/lib/home-site-stack.ts
+++ cdk/lib/home-site-stack.ts
@@ -1,11 +1,23 @@
 import * as cdk from '@aws-cdk/core';
-import * as s3 from '@aws-cdk/aws-s3';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as ecs_patterns from '@aws-cdk/aws-ecs-patterns';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, "dewei-test-s3bucket");
+    const vpc = new ec2.Vpc(this, "VPC");
+
+    const cluster = new ecs.Cluster(this, "Cluster", { vpc });
+
+
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
+      cluster: cluster,
+      taskImageOptions: { image: ecs.ContainerImage.fromRegistry("deweiliu/home-site:latest") },
+      publicLoadBalancer: true
+    });
+
 
 
   }
