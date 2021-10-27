da15094d2432b2ee435138ca5ea7a0d25535cde2
Dewei Liu <42348814+deweiliu@users.noreply.github.com>
2021-10-27 02:27:05 +0100

    Remove Cname

--- cdk/lib/home-site-stack.ts
+++ cdk/lib/home-site-stack.ts
@@ -5,7 +5,7 @@ import * as ecs_patterns from '@aws-cdk/aws-ecs-patterns';
 import * as route53 from '@aws-cdk/aws-route53';
 import { Duration } from '@aws-cdk/core';
 import * as acm from '@aws-cdk/aws-certificatemanager';
-import {ApplicationProtocol} from '@aws-cdk/aws-elasticloadbalancingv2';
+import { ApplicationProtocol } from '@aws-cdk/aws-elasticloadbalancingv2';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -20,9 +20,7 @@ export class CdkStack extends cdk.Stack {
     });
 
     const vpc = new ec2.Vpc(this, "VPC");
-
     const cluster = new ecs.Cluster(this, "Cluster", { vpc });
-
 
     const fargate = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
       cluster: cluster,
@@ -34,15 +32,12 @@ export class CdkStack extends cdk.Stack {
     });
 
 
-    new route53.CnameRecord(this, 'CName', {
-      domainName,
-      zone: hostedZone,
-      recordName: fargate.loadBalancer.loadBalancerDnsName,
-      ttl: Duration.hours(1),
-    });
-
-
-
+    // new route53.CnameRecord(this, 'CName', {
+    //   domainName,
+    //   zone: hostedZone.zoneName,
+    //   recordName: fargate.loadBalancer.loadBalancerDnsName,
+    //   ttl: Duration.hours(1),
+    // });
 
   }
 }
