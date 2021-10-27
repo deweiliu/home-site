875e6cd4f8ecdaf18f6301d0f08ecb857a40ce67
Dewei Liu <42348814+deweiliu@users.noreply.github.com>
2021-10-27 02:22:41 +0100

    Add SSL Certificate

--- cdk/lib/home-site-stack.ts
+++ cdk/lib/home-site-stack.ts
@@ -4,10 +4,20 @@ import * as ecs from '@aws-cdk/aws-ecs';
 import * as ecs_patterns from '@aws-cdk/aws-ecs-patterns';
 import * as route53 from '@aws-cdk/aws-route53';
 import { Duration } from '@aws-cdk/core';
+import * as acm from '@aws-cdk/aws-certificatemanager';
+import {ApplicationProtocol} from '@aws-cdk/aws-elasticloadbalancingv2';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+
+    const domainName = 'dliu.com';
+    const hostedZone = route53.HostedZone.fromHostedZoneId(this, 'HostedZone', cdk.Fn.importValue('DLIUCOMHostedZoneID').toString());
+    const certificate = new acm.Certificate(this, 'Certificate', {
+      domainName,
+      validation: acm.CertificateValidation.fromDns(hostedZone),
+    });
 
     const vpc = new ec2.Vpc(this, "VPC");
 
@@ -17,18 +27,20 @@ export class CdkStack extends cdk.Stack {
     const fargate = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
       cluster: cluster,
       taskImageOptions: { image: ecs.ContainerImage.fromRegistry("deweiliu/home-site:latest") },
-      publicLoadBalancer: true
+      publicLoadBalancer: true,
+      protocol: ApplicationProtocol.HTTPS,
+      redirectHTTP: true,
+      certificate
     });
 
+
     new route53.CnameRecord(this, 'CName', {
-      domainName: 'dliu.com',
-      zone: route53.HostedZone.fromHostedZoneId(this, 'HostedZone', cdk.Fn.importValue('DLIUCOMHostedZoneID').toString()),
+      domainName,
+      zone: hostedZone,
       recordName: fargate.loadBalancer.loadBalancerDnsName,
       ttl: Duration.hours(1),
     });
 
-
-    // new cdk.CfnOutput(this,fargate.loadBalancer.loadBalancerDnsName)
 
 
 
