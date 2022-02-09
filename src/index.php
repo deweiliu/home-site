fc735f839f036efc1a0dd1ee65620c343dc3219a
Dewei Liu <42348814+deweiliu@users.noreply.github.com>
2022-02-09 04:09:51 +0000

    Use links to file server instead of including static files

--- src/index.php
+++ src/index.php
@@ -6,7 +6,7 @@
     <title>Dewei Liu</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="icon" type="image/png" href="images/favicon.png" />
+    <link rel="icon" type="image/png" href="https://files.dliu.com/dliu.com/favicon.png" />
     <meta name="theme-color" content="#40B1FA">
 </head>
 
@@ -21,13 +21,14 @@
         <h1><a href="/">Dewei Liu</a></h1>
 
         <p>Hi, welcome to my personal website.</p>
-        <p>Please view my <a href="doc/resume.pdf">resume</a> and my recent <a href="https://ieeexplore.ieee.org/document/9560107">publication on IEEE</a>.</p>
+        <p>Please view my <a href="https://files.dliu.com/me/resume.pdf">resume</a> and my recent <a href="https://ieeexplore.ieee.org/document/9560107">publication on IEEE</a>.</p>
         <p>Below are my on-going projects</p>
         <p><a href="https://jenkins.dliu.com">Jenkins Pipelines</a> - I manage the entire Jenkins CI/CD environment hosted on AWS. The environment is defined as Infrastructure as Code (IaC) and I made it <a href="https://github.com/deweiliu/jenkins">avilable to public</a>.</p>
         <p><a href="https://bfa.dliu.com/">Best Friends App</a> - An online chatbot that does simple
             conversations</p>
         <p><a href="https://paperborne.dliu.com/">Paperborne</a> - An Android card game with an online demo.</p>
         <p><a href="https://dev.dliu.com/">Developer Wiki</a> - A wiki that documents how my websites are configured and how they work.</p>
+        <p><a href="https://files.dliu.com/">File Server</a> - A file server that servers static files to my other websites, powered by Apache httpd.</p>
         <br>
         <p>Below are my games</p>
         <p><a href="https://rps.deweiliu.com/">Rock Paper Scissors</a></p>
@@ -45,7 +46,7 @@
         <br>
 
         <p>Thank you :)</p>
-        <img src="/images/favicon.png" width="50px" height="50px">
+        <img src="https://files.dliu.com/dliu.com/favicon.png" width="50px" height="50px">
     </div>
 </body>
 
